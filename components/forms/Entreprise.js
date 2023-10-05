fb4f4530e4d361d32d5e5da646f2482e6c555389
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-10-05 14:25:19 +0200

    Update Entreprise.js

--- components/forms/Entreprise.js
+++ components/forms/Entreprise.js
@@ -1,10 +1,18 @@
 import React from "react";
 import ButtonWithBorder from "../button/ButtonWithBorder";
+import ButtonGreen from "../button/ButtonGreen";
+import Input from "./input/Input";
+import CheckBox from "./checkbox/CheckBox";
 
 const Entreprise = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const data = new FormData(e.target);
+    console.log(data);
+  };
   return (
-    <section className="w-full max-w-[1250px] px-0 py-0 lg:px-20 lg:py-10 mx-auto flex justify-between items-stretch lg:flex-row flex-col bg-light">
-      <div className="w-5/12">
+    <section className="w-full max-w-[1250px] px-0 py-0 lg:px-20 lg:py-10 mx-auto flex justify-between items-center lg:flex-row flex-col bg-light">
+      <div className="w-full lg:w-5/12 lg:py-0 py-10 px-6">
         <h2 className="text-3xl text-dark font-semibold">
           Comment devenir partenaire MaPrimeRenov-Info.org ?
         </h2>
@@ -31,7 +39,46 @@ const Entreprise = () => {
           </div>
         </div>
       </div>
-      <form className="w-6/12 bg-white shadow-md py-10 px-5"></form>
+      <form
+        className="w-full lg:w-6/12 bg-white lg:shadow-md py-10 px-5"
+        onSubmit={handleSubmit}
+      >
+        <h2 className="text-3xl text-dark font-semibold mx-auto">
+          Devenir Partnaire
+        </h2>
+        <h3 className="text-xl text-dark font-semibold mt-10">
+          Information de l’entreprise:
+        </h3>
+        <div className="w-full mt-7">
+          <div className="flex justify-between">
+            <Input label="Nom de l’entreprise*" />
+            <Input label="Siren*" />
+          </div>
+          <div className="flex justify-between mt-5">
+            <Input label="Type d'activité*" />
+            <CheckBox label="Je suis une entreprise certifié RGE*" />
+          </div>
+        </div>
+        <h3 className="text-xl text-dark font-semibold mt-10">
+          Information de l’entreprise:
+        </h3>
+        <div className="w-full mt-7">
+          <div className="flex justify-between">
+            <Input label="Nom, prénom*" />
+            <Input label="Email*" />
+          </div>
+          <div className="flex justify-between mt-5">
+            <Input label="Téléphone*" />
+            <Input label="Code postal*" />
+          </div>
+        </div>
+        <div className="mt-7">
+          <CheckBox />
+        </div>
+        <div className="mt-7 w-full flex justify-center">
+          <ButtonGreen value="Envoyer" />
+        </div>
+      </form>
     </section>
   );
 };
