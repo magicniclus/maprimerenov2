f50aa7afcc54eedcf5e625360eeb0788fc7b975d
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-10-05 12:28:04 +0200

    Update FAQ.js

--- components/FAQ/FAQ.js
+++ components/FAQ/FAQ.js
@@ -3,36 +3,85 @@ import { Disclosure } from "@headlessui/react";
 import { MinusSmallIcon, PlusSmallIcon } from "@heroicons/react/24/outline";
 const faqs = [
   {
-    question: "What's the best thing about Switzerland?",
+    question:
+      "Comment fonctionne le service de mise en relation avec des entreprises?",
     answer:
-      "I don't know, but the flag is a big plus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.",
+      "Notre plateforme utilise des algorithmes avancés pour connecter les utilisateurs avec des entreprises adaptées à leurs besoins. En quelques clics, vous pouvez être mis en relation avec des entreprises de confiance dans votre domaine.",
   },
   {
-    question: "What's the best thing about Switzerland?",
+    question:
+      "Est-ce que votre service est gratuit pour les chercheurs d'entreprise?",
     answer:
-      "I don't know, but the flag is a big plus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.",
+      "Oui, notre service est entièrement gratuit pour ceux qui recherchent des entreprises. Nous sommes financés par les entreprises partenaires qui souhaitent augmenter leur visibilité.",
   },
   {
-    question: "What's the best thing about Switzerland?",
+    question:
+      "Comment garantissez-vous la qualité des entreprises sur votre plateforme?",
     answer:
-      "I don't know, but the flag is a big plus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.",
+      "Chaque entreprise sur notre plateforme est soigneusement vérifiée selon des critères stricts de qualité, d'expérience et de fiabilité pour assurer la satisfaction de nos utilisateurs.",
   },
   {
-    question: "What's the best thing about Switzerland?",
+    question: "Puis-je consulter les avis sur les entreprises?",
     answer:
-      "I don't know, but the flag is a big plus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.",
+      "Absolument! Vous pouvez consulter les avis et témoignages laissés par d'autres utilisateurs pour chaque entreprise afin de faire un choix éclairé.",
   },
   {
-    question: "What's the best thing about Switzerland?",
+    question: "Comment puis-je inscrire mon entreprise sur votre plateforme?",
     answer:
-      "I don't know, but the flag is a big plus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.",
+      "C'est simple! Cliquez sur la section 'Inscrire mon entreprise' et suivez les instructions pour ajouter votre entreprise à notre liste de partenaires.",
   },
   {
-    question: "What's the best thing about Switzerland?",
+    question:
+      "Proposez-vous des partenariats ou des collaborations avec des entreprises?",
     answer:
-      "I don't know, but the flag is a big plus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.",
+      "Oui, nous sommes toujours ouverts aux collaborations et partenariats. Contactez-nous pour discuter des opportunités et des avantages mutuels.",
+  },
+  {
+    question:
+      "En combien de temps puis-je être mis en relation avec une entreprise?",
+    answer:
+      "Dès que vous faites une demande, notre système commence à chercher la meilleure correspondance. La plupart des utilisateurs sont mis en relation en moins de 24 heures.",
+  },
+  {
+    question: "Votre plateforme est-elle disponible internationalement?",
+    answer:
+      "Actuellement, nous opérons principalement dans certaines régions, mais nous avons pour ambition d'étendre nos services à l'international prochainement.",
+  },
+  {
+    question:
+      "Comment garantissez-vous la confidentialité de mes informations?",
+    answer:
+      "La confidentialité est primordiale pour nous. Vos informations ne sont utilisées que pour la mise en relation et ne sont jamais partagées avec des tiers sans votre consentement.",
+  },
+  {
+    question: "Puis-je annuler une demande de mise en relation?",
+    answer:
+      "Bien sûr! Si vous changez d'avis, vous pouvez annuler votre demande à tout moment depuis votre tableau de bord.",
+  },
+  {
+    question:
+      "Quels sont les avantages de trouver une entreprise via votre plateforme plutôt que de chercher moi-même?",
+    answer:
+      "Grâce à notre plateforme, vous gagnez du temps, bénéficiez d'une sélection de qualité et avez accès aux avis et témoignages d'autres utilisateurs pour faciliter votre choix.",
+  },
+  {
+    question: "Puis-je recommander une entreprise à un ami via votre site?",
+    answer:
+      "Oui, avec notre système de parrainage, vous pouvez recommander facilement des entreprises à vos amis et votre famille.",
+  },
+  {
+    question:
+      "Y a-t-il une garantie si je ne suis pas satisfait de l'entreprise avec laquelle j'ai été mis en relation?",
+    answer:
+      "La satisfaction de nos utilisateurs est notre priorité. Si vous rencontrez des problèmes, contactez-nous, et nous ferons de notre mieux pour vous aider.",
+  },
+  {
+    question: "Puis-je consulter votre plateforme sur mobile?",
+    answer:
+      "Absolument! Notre site est optimisé pour tous les appareils, que ce soit un ordinateur, une tablette ou un smartphone. Vous pouvez donc nous consulter où que vous soyez.",
   },
 ];
+
 const FAQ = () => {
   return (
     <div className="bg-white">
