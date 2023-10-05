f56453a758ada8a528b3c1f900880307f96e0444
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-10-05 12:28:08 +0200

    Create Entreprise.js

---
+++ components/forms/Entreprise.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Entreprise = () => {
+  return (
+    <section className="w-full max-w-[1250px] px-0 py-0 lg:px-20 lg:py-10 mx-auto flex justify-between items-stretch lg:flex-row flex-col bg-light"></section>
+  );
+};
+
+export default Entreprise;
