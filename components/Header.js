36fe756ef6974ec2d608262503a5948fb97c8822
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-10-05 12:28:11 +0200

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -3,8 +3,8 @@ import ButtonWithBorder from "./button/ButtonWithBorder";
 
 const Header = () => {
   return (
-    <header className="w-full max-w-[1250px] px-5 py-10 lg:px-20 lg:py-8 mx-auto flex md:flex-row flex-col justify-between items-center shadow-sm">
-      <a className="text-primary text-lg" href="/">
+    <header className="w-full max-w-[1250px] px-5 py-10 lg:px-20 lg:py-8 mx-auto flex md:flex-row flex-col justify-between items-center shadow-sm z-50">
+      <a className="text-primary text-2xl" href="/">
         Maprimerenov-info.org
       </a>
       <div className="flex items-center md:mt-0 mt-7">
