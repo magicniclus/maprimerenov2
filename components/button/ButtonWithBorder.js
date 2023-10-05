f8f97a18a667c5c517742739c7d900e53ddcc06e
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-10-05 13:47:12 +0200

    Update ButtonWithBorder.js

--- components/button/ButtonWithBorder.js
+++ components/button/ButtonWithBorder.js
@@ -5,7 +5,7 @@ const ButtonWithBorder = (props) => {
   const value = props.value;
   return (
     <a
-      className="text-primary text-sm border border-primary rounded-md px-4 py-2 ml-5 text-center max-w-[170px] hover:bg-primary hover:bg-opacity-10 transition-all duration-300 ease-in-out"
+      className="text-primary text-sm border border-primary rounded-md px-4 py-2 text-center max-w-[170px] hover:bg-primary hover:bg-opacity-10 transition-all duration-300 ease-in-out"
       href={link || "#"}
     >
       {value || "VOUS ÊTES UN PROFESSIONNEL ?"}
