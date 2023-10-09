9b05ae59862beba7242beff85cc2efb551153aab
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-10-09 10:52:45 +0200

    Create Simulateur.js

---
+++ components/forms/simulateur/Simulateur.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Simulateur = () => {
+  return (
+    <div className="min-h-[700px] w-full bg-light rounded-md shadow-md px-10 py-5 flex flex-col items-center">
+      <h2 className="text-3xl text-dark font-semibold">
+        Testez votre éligibilité
+      </h2>
+      <img
+        src="/images/icons/Drapeau.png"
+        alt="arrow"
+        className="mt-5 w-full"
+      />
+    </div>
+  );
+};
+
+export default Simulateur;
