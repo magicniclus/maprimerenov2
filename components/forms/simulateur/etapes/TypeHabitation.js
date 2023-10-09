75bf4e9ef4e9ece76f2feb33690e4bc049da4037
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-10-09 13:18:38 +0200

    Update TypeHabitation.js

--- components/forms/simulateur/etapes/TypeHabitation.js
+++ components/forms/simulateur/etapes/TypeHabitation.js
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+import FormLanding from "../../../../layout/FormLanding";
+import CardWithImage from "../cards/CardWithImage";
 
 const TypeHabitation = () => {
-  return <div></div>;
+  const [cardSelected, setCardSelected] = useState(null);
+  const clickManager = (newValue) => {
+    if (cardSelected === newValue) {
+      setCardSelected(null);
+    } else {
+      setCardSelected(newValue);
+    }
+  };
+  return (
+    <FormLanding title="Votre projet concerne:*">
+      <CardWithImage
+        src="/images/icons/maison.png"
+        alt="maison"
+        title="Maison"
+        onClick={() => clickManager(1)}
+        selectedValue={cardSelected}
+        value={1}
+      />
+      <CardWithImage
+        src="/images/icons/immeuble.png"
+        alt="immeuble"
+        title="Immeuble"
+        onClick={() => clickManager(2)}
+        selectedValue={cardSelected}
+        value={2}
+      />
+    </FormLanding>
+  );
 };
 
 export default TypeHabitation;
