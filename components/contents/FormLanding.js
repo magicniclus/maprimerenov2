cd2ddf2c4f412e0479f922323d4c1a0c211f49d7
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-10-09 10:52:34 +0200

    Update FormLanding.js

--- components/contents/FormLanding.js
+++ components/contents/FormLanding.js
@@ -1,31 +1,32 @@
 import React from "react";
-import Simulateur from "../forms/Simulateur";
+import Simulateur from "../forms/simulateur/Simulateur";
 
 const FormLanding = () => {
   return (
     <section className="w-full max-w-[1250px] px-0 py-0 lg:px-20 lg:py-10 mx-auto flex justify-between items-stretch lg:flex-row flex-col">
       <div className="lg:w-4/12 w-full flex-col justify-center min-h-[100%] lg:flex hidden lg:px-0 px-5">
         <div>
-          <h3 className="text-3xl text-dark font-semibold">
+          <h2 className="text-3xl text-dark font-semibold">
             MaPrimeRenov’, c’est jusqu’à 20.000€ de prise en charge par l’état !
-          </h3>
+          </h2>
           <p className="mt-5 text-dark">
             Bénéficiez du montant maximum subventionné pour vous. Il vous suffit
             de tester l’éligibilité de votre projet ! Utilisez notre simulateur
-            GRATUIT et sans conditions de revenus pour tester votre éligibilité
-            dès maintenant.
+            MaPrimeRenov’ GRATUIT et sans conditions de revenus pour tester
+            votre éligibilité dès maintenant.
           </p>
         </div>
         <div className="md:mt-32 mt-10">
-          <h3 className="text-3xl text-dark font-semibold">
+          <h2 className="text-3xl text-dark font-semibold">
             Qui peut en bénéficier ?
-          </h3>
+          </h2>
           <p className="mt-5 text-dark">
             Depuis le 1er octobre 2020, tous les propriétaires peuvent en
             bénéficier, quels que soient leurs revenus. Les propriétaires
             bailleurs peuvent déposer leur dossier de demande d’aide depuis le
             1er juillet 2021. En 2021 plus de 644 000 demandes d’aide ont été
-            accordé par l’État.
+            accordé par l’État. Utilisez notre simulateur MaPrimeRenov’ pour
+            connaître votre éligibilité.
           </p>
         </div>
       </div>
@@ -34,9 +35,9 @@ const FormLanding = () => {
       </div>
       <div className="lg:w-4/12 w-full flex-col justify-center min-h-[100%] lg:hidden flex mt-10 lg:mt-20 lg:px-0 px-5">
         <div>
-          <h3 className="text-xl text-dark font-semibold">
+          <h2 className="text-xl text-dark font-semibold">
             MaPrimeRenov’, c’est jusqu’à 20.000€ de prise en charge par l’état !
-          </h3>
+          </h2>
           <p className="mt-5 text-xs text-dark">
             Bénéficiez du montant maximum subventionné pour vous. Il vous suffit
             de tester l’éligibilité de votre projet ! Utilisez notre simulateur
@@ -45,9 +46,9 @@ const FormLanding = () => {
           </p>
         </div>
         <div className="lg:mt-24 mt-10">
-          <h3 className="text-xl text-dark font-semibold">
+          <h2 className="text-xl text-dark font-semibold">
             Qui peut en bénéficier ?
-          </h3>
+          </h2>
           <p className="mt-5 text-xs text-dark">
             Depuis le 1er octobre 2020, tous les propriétaires peuvent en
             bénéficier, quels que soient leurs revenus. Les propriétaires
