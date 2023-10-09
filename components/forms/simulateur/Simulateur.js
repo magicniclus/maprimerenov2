5cc0c9028933e45971eec8adf904c88029e00874
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-10-09 13:18:42 +0200

    Update Simulateur.js

--- components/forms/simulateur/Simulateur.js
+++ components/forms/simulateur/Simulateur.js
@@ -1,16 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
+import TypeHabitation from "./etapes/TypeHabitation";
+import AnneeDeConstruction from "./etapes/AnneeDeConstruction";
+import Surface from "./etapes/Surface";
 
 const Simulateur = () => {
+  const [card, setCard] = useState(0);
+
+  const updateCards = () => {
+    switch (card) {
+      case 0:
+        return <TypeHabitation />;
+
+      case 1:
+        return <AnneeDeConstruction />;
+
+      case 2:
+        return <Surface />;
+
+      default:
+        <TypeHabitation />;
+        break;
+    }
+  };
+
+  const buttonManager = () => {
+    return (
+      <div className="flex justify-between mt-10 flex-col">
+        <button
+          className="bg-green text-white rounded-md px-5 py-2 font-semibold"
+          onClick={() => setCard((card) => card + 1)}
+        >
+          Suivant
+        </button>
+        <button
+          className="text-dark/50 border border-dark/50 rounded-md px-5 py-2 font-semibold mt-3"
+          onClick={() => setCard((card) => card - 1)}
+        >
+          Précédent
+        </button>
+      </div>
+    );
+  };
+
   return (
-    <div className="min-h-[700px] w-full bg-light rounded-md shadow-md px-10 py-5 flex flex-col items-center">
-      <h2 className="text-3xl text-dark font-semibold">
-        Testez votre éligibilité
-      </h2>
-      <img
-        src="/images/icons/Drapeau.png"
-        alt="arrow"
-        className="mt-5 w-full"
-      />
+    <div className="min-h-[700px] w-full bg-light rounded-md shadow-md px-10 py-5 flex flex-col items-center justify-between">
+      <div className="flex flex-col items-center">
+        <h2 className="text-3xl text-dark font-semibold">
+          Testez votre éligibilité
+        </h2>
+        <img
+          src="/images/icons/Drapeau.png"
+          alt="arrow"
+          className="mt-5 w-full"
+        />
+      </div>
+      {updateCards()}
+      {buttonManager()}
     </div>
   );
 };
