c161e763663f8e2cff1141730324a7aa409a6761
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-10-09 13:18:45 +0200

    Create FormLanding.js

---
+++ layout/FormLanding.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const FormLanding = (props) => {
+  return (
+    <div className="flex flex-col items-center w-full">
+      <h3 className="mt-8 mb-2 text-dark text-md font-bold ">{props.title}</h3>
+      {props.description && (
+        <p className="text-center text-xs text-dark/50 mb-8">
+          {props.description}
+        </p>
+      )}
+      <div className="flex min-w-full justify-around flex-wrap">
+        {props.children}
+      </div>
+    </div>
+  );
+};
+
+export default FormLanding;
