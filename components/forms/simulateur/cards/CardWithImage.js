16a7285b284309333ab2c29c1b306f55476949ce
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-10-09 13:18:32 +0200

    Update CardWithImage.js

--- components/forms/simulateur/cards/CardWithImage.js
+++ components/forms/simulateur/cards/CardWithImage.js
@@ -1,7 +1,38 @@
-import React from "react";
+import { CheckIcon } from "@heroicons/react/20/solid";
 
-const CardWithImage = () => {
-  return <div></div>;
+const CardWithImage = ({
+  src,
+  alt,
+  title,
+  onClick,
+  value,
+  selectedValue,
+  margin,
+}) => {
+  const isSelected = value === selectedValue;
+
+  return (
+    <div
+      className={`relative flex flex-col items-center justify-between py-10 w-40 h-48 bg-white hover:shadow-md transition-all duration-300 ease-in-out rounded-md ${
+        margin ? "mr-3" : null
+      } ${isSelected ? "border border-primary" : "border-2 border-light"}`}
+      onClick={onClick}
+    >
+      <div
+        className={`w-5 h-5 rounded-full absolute top-3 right-3 border border-dark transition-all duration-300 ease-in-out ${
+          isSelected ? "border-2 bg-green" : ""
+        }`}
+      >
+        {isSelected && (
+          <div className="w-full h-full flex justify-center items-center">
+            <CheckIcon className="text-dark" />
+          </div>
+        )}
+      </div>
+      <img src={src} alt={alt} />
+      <p className="mt-3 font-light">{title}</p>
+    </div>
+  );
 };
 
 export default CardWithImage;
