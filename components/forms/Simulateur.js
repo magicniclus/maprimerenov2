b732f86996112c7649e9f6b2f87c02bdfaefa48e
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-10-09 10:52:38 +0200

    Delete Simulateur.js

--- components/forms/Simulateur.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Simulateur = () => {
-  return (
-    <div className="min-h-[700px] w-full bg-light rounded-md shadow-md"></div>
-  );
-};
-
-export default Simulateur;
