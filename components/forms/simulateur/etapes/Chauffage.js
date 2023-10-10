6a17316901ade7af5d9357421b6a3dc362022b79
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-10-10 14:01:41 +0200

    Update Chauffage.js

--- components/forms/simulateur/etapes/Chauffage.js
+++ components/forms/simulateur/etapes/Chauffage.js
@@ -1,9 +1,13 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import FormLanding from "../../../../layout/FormLanding";
 import LittleCardWithImage from "../cards/LittleCardWithImage";
+import { useDispatch } from "react-redux";
 
 const Chauffage = () => {
+  const dispatch = useDispatch();
+
   const [cardSelected, setCardSelected] = useState(null);
+
   const clickManager = (newValue) => {
     if (cardSelected === newValue) {
       setCardSelected(null);
@@ -11,6 +15,33 @@ const Chauffage = () => {
       setCardSelected(newValue);
     }
   };
+
+  const translateCardSelected = () => {
+    switch (cardSelected) {
+      case 1:
+        return "Electrique";
+      case 2:
+        return "Fioul";
+      case 3:
+        return "Gaz";
+      case 4:
+        return "Feu";
+      case 5:
+        return "Pompe à chaleur";
+      case 6:
+        return "Charbon";
+      default:
+        return "";
+    }
+  };
+
+  useEffect(() => {
+    dispatch({
+      type: "UPDATE_USER_INFORMATIONS",
+      payload: { typeDeChauffage: translateCardSelected() },
+    });
+  }, [cardSelected]);
+
   return (
     <FormLanding title="Quel est votre principale source de chauffage actuellement :*">
       <LittleCardWithImage
@@ -46,9 +77,9 @@ const Chauffage = () => {
         value={4}
       />
       <LittleCardWithImage
-        src="/images/icons/chauffage.png"
-        alt="chauffage"
-        title="Chauffage"
+        src="/images/icons/pompe.png"
+        alt="Pompe à chaleur"
+        title="Pompe à chaleur"
         onClick={() => clickManager(5)}
         selectedValue={cardSelected}
         value={5}
