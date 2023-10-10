730cf8a4ea634834cfdfc0088a55852d9680109b
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-10-10 10:58:24 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,21 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { Provider } from "react-redux";
+import store from "../redux/store";
+import TagManager from "react-gtm-module";
+import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    const tagManagerArgs = {
+      // gtmId: "GTM-WP9R782",
+    };
+    TagManager.initialize(tagManagerArgs);
+  }, []);
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
 
-export default MyApp
+export default MyApp;
