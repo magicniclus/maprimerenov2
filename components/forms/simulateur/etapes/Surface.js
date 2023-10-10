28af54fda6ec68ea7f2c26f23fd4a5e45469b58b
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-10-10 14:01:50 +0200

    Update Surface.js

--- components/forms/simulateur/etapes/Surface.js
+++ components/forms/simulateur/etapes/Surface.js
@@ -1,9 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import FormLanding from "../../../../layout/FormLanding";
 import WhiteInput from "../../input/WhiteInput";
+import { useDispatch } from "react-redux";
 
 const Surface = () => {
+  const dispatch = useDispatch();
+
   const [surface, setSurface] = useState(null);
+
+  useEffect(() => {
+    dispatch({
+      type: "UPDATE_USER_INFORMATIONS",
+      payload: { surface },
+    });
+  }, [surface]);
+
   return (
     <FormLanding
       title="Surface habitable de votre logement :*"
