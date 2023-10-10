9aefaec9a3fb3f16c00612dccc3d1d5cf13ae71a
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-10-10 14:01:39 +0200

    Update AnneeDeConstruction.js

--- components/forms/simulateur/etapes/AnneeDeConstruction.js
+++ components/forms/simulateur/etapes/AnneeDeConstruction.js
@@ -1,8 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import FormLanding from "../../../../layout/FormLanding";
 import CardWithImage from "../cards/CardWithImage";
+import { useDispatch } from "react-redux";
 
 const AnneeDeConstruction = () => {
+  const dispatch = useDispatch();
+
   const [cardSelected, setCardSelected] = useState(null);
   const clickManager = (newValue) => {
     if (cardSelected === newValue) {
@@ -11,6 +14,27 @@ const AnneeDeConstruction = () => {
       setCardSelected(newValue);
     }
   };
+
+  const translateCardSelected = () => {
+    switch (cardSelected) {
+      case 1:
+        return "Moins de deux ans";
+      case 2:
+        return "Entre deux et 10 ans";
+      case 3:
+        return "Plus de 10 ans";
+      default:
+        return "";
+    }
+  };
+
+  useEffect(() => {
+    dispatch({
+      type: "UPDATE_USER_INFORMATIONS",
+      payload: { ageDuBien: translateCardSelected() },
+    });
+  }, [cardSelected]);
+
   return (
     <FormLanding
       title="Année de construction de votre logement :*"
@@ -19,24 +43,24 @@ Cela nous permet de determiner votre éligibilité aux différentes aides"
     >
       <CardWithImage
         src="/images/icons/calendrier.png"
-        alt="maison"
-        title="Maison"
+        alt="Moins de deux ans"
+        title="Moins de deux ans"
         onClick={() => clickManager(1)}
         selectedValue={cardSelected}
         value={1}
       />
       <CardWithImage
         src="/images/icons/calendrier.png"
-        alt="maison"
-        title="Maison"
+        alt="Entre deux et 10 ans"
+        title="Entre deux et 10 ans"
         onClick={() => clickManager(2)}
         selectedValue={cardSelected}
         value={2}
       />
       <CardWithImage
         src="/images/icons/calendrier.png"
-        alt="maison"
-        title="Maison"
+        alt="Plus de 10 ans"
+        title="Plus de 10 ans"
         onClick={() => clickManager(3)}
         selectedValue={cardSelected}
         value={3}
