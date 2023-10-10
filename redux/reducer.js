508de7365100f2afad9811fe27bc837352c927aa
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-10-10 14:02:01 +0200

    Update reducer.js

--- redux/reducer.js
+++ redux/reducer.js
@@ -1,13 +1,23 @@
-const initState = { count: 0 };
+const initState = { userInformation: {}, step: 0 };
 
 const reducer = (state = initState, action) => {
   switch (action.type) {
-    case "SET_STATE":
+    case "UPDATE_USER_INFORMATIONS":
       return {
         ...state,
-        ...action.payload,
+        userInformation: {
+          ...state.userInformation,
+          ...action.payload,
+        },
+      };
+    case "UPDATE_STEP":
+      return {
+        ...state,
+        step: action.payload,
       };
     default:
       return state;
   }
 };
+
+export default reducer;
