a5f1dc89318fd853b774f0be0ea67287e99f210f
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-10-10 10:10:42 +0200

    Update Inscription.js

--- components/forms/simulateur/etapes/Inscription.js
+++ components/forms/simulateur/etapes/Inscription.js
@@ -15,40 +15,46 @@ const Inscription = () => {
       title="Comment vous appelez-vous ?*"
       description="Vous acceptez d'être rappelé gratuitement par un de nos expert afin de faire avancer votre dossier"
     >
-      <div className="flex flex-col mt-2">
-        <label className="mb-1" for="fname">
-          NOM, Prénom *
-        </label>
-        <WhiteInput type="text" value={nameValue} setValue={setNameValue} />
-      </div>
-      <div className="flex flex-col mt-2">
-        <label className="mb-1" for="phone">
-          Téléphone*
-        </label>
-        <WhiteInput
-          type="phone"
-          value={telephoneValue}
-          setValue={setTelephoneValue}
-        />
-      </div>
-      <div className="flex flex-col mt-2">
-        <label className="mb-1" for="email">
-          Email *
-        </label>
-        <WhiteInput type="email" value={emailValue} setValue={setEmailValue} />
-      </div>
-      <div className="flex flex-col mt-2">
-        <label className="mb-1" for="fname">
-          NOM, Prénom *
-        </label>
-        <WhiteSelect
-          options={options}
-          value={selectValue}
-          setValue={setSelectValue}
-        />
-      </div>
-      <div className="mt-3 max-w-[400px]">
-        <CheckBox label="J'accepte les conditions de traitement des données personnelles conformément au RGPD." />
+      <div className="flex flex-col">
+        <div className="flex flex-col">
+          <label className="mb-1" for="fname">
+            NOM, Prénom *
+          </label>
+          <WhiteInput type="text" value={nameValue} setValue={setNameValue} />
+        </div>
+        <div className="flex flex-col mt-2">
+          <label className="mb-1" for="phone">
+            Téléphone*
+          </label>
+          <WhiteInput
+            type="phone"
+            value={telephoneValue}
+            setValue={setTelephoneValue}
+          />
+        </div>
+        <div className="flex flex-col mt-2">
+          <label className="mb-1" for="email">
+            Email *
+          </label>
+          <WhiteInput
+            type="email"
+            value={emailValue}
+            setValue={setEmailValue}
+          />
+        </div>
+        <div className="flex flex-col mt-2">
+          <label className="mb-1" for="fname">
+            NOM, Prénom *
+          </label>
+          <WhiteSelect
+            options={options}
+            value={selectValue}
+            setValue={setSelectValue}
+          />
+        </div>
+        <div className="mt-3 max-w-[450px]">
+          <CheckBox label="J'accepte les conditions de traitement des données personnelles conformément au RGPD." />
+        </div>
       </div>
     </FormLanding>
   );
