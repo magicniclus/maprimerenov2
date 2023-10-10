69c68076a305b2ab1ab4ed8aae5cd968cd5fa1be
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-10-10 14:38:28 +0200

    Update ProgressBar.js

--- components/forms/simulateur/ProgressBar.js
+++ components/forms/simulateur/ProgressBar.js
@@ -5,11 +5,18 @@ const ProgressBar = () => {
   const getStep = useSelector((state) => state.step);
 
   return (
-    <div className="w-full h-2 rounded-md bg-gray-200 relative">
+    <div className="w-full h-3 rounded-md bg-gray-200 relative">
       <div
         className={` bg-green absolute rounded-md h-[80%] top-1/2 transform -translate-y-1/2 transition-all duration-500 ease-out`}
         style={{ width: `calc(100% / 7 * ${getStep})` }}
       />
+      <p
+        className={`text-white text-[8px] absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2 transition-all duration-500 ease-out font-bold ${
+          getStep === 7 ? "opacity-100" : "opacity-0"
+        }`}
+      >
+        Derniere étape !
+      </p>
     </div>
   );
 };
