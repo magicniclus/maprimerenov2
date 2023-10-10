6b0f02e90ecdc3cfa38fb3c9ed04cf71141de685
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-10-10 14:01:35 +0200

    Update CheckBox.js

--- components/forms/checkbox/CheckBox.js
+++ components/forms/checkbox/CheckBox.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const CheckBox = (props) => {
+const CheckBox = ({ label, setValue }) => {
   return (
     <div className="relative flex items-start w-[47%]">
       <div className="flex h-6 items-center">
@@ -10,10 +10,11 @@ const CheckBox = (props) => {
           name="offers"
           type="checkbox"
           className="h-4 w-4 rounded border-dark text-primary focus:ring-primary"
+          onChange={(e) => setValue(e.target.checked)}
         />
       </div>
       <div className="ml-3 text-xs text-dark/80 flex items-center">
-        {props.label || "J'accepte de recevoir des offres promotionnelles"}
+        {label || "J'accepte de recevoir des offres promotionnelles"}
       </div>
     </div>
   );
