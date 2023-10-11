186410033e9566108e87b56a66120ffbb419ce79
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-10-11 11:24:11 +0200

    Update ButtonGreen.js

--- components/button/ButtonGreen.js
+++ components/button/ButtonGreen.js
@@ -2,12 +2,16 @@ import React from "react";
 
 const ButtonGreen = (props) => {
   return (
-    <a
+    <button
+      type={props.type || "button"}
       href={props.href || "#"}
-      className="cursor-pointer rounded-md bg-green px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-green/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green"
+      className={`cursor-pointer rounded-md bg-green px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-green/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green ${
+        props.disabled ? "opacity-50" : ""
+      }`}
+      disabled={props.disabled || false}
     >
       {props.value || "Envoyer"}
-    </a>
+    </button>
   );
 };
 
