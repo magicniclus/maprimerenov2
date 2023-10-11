689d5ed00e8e7e74d4750e911c6b54607b37ec38
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-10-11 11:13:20 +0200

    Create getDate.js

---
+++ utils/getDate.js
@@ -0,0 +1,13 @@
+export const updateDate = () => {
+  const date = new Date();
+  const options = {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    hour12: false,
+  };
+  return date.toLocaleDateString("fr-FR", options);
+};
