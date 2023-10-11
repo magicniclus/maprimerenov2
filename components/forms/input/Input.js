77eefe7b1231fda0a8acf1765b3669bafeb8f411
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-10-11 11:13:04 +0200

    Update Input.js

--- components/forms/input/Input.js
+++ components/forms/input/Input.js
@@ -1,20 +1,23 @@
 import React from "react";
 
 const Input = (props) => {
+  const { label = "Nom", id, name, value, onChange } = props;
+
   return (
     <div className="relative w-[47%]">
       <label
-        htmlFor="name"
+        htmlFor={id}
         className="absolute -top-2 left-2 inline-block bg-white px-1 text-xs font-medium text-dark"
       >
-        {props.label || "Nom"}
+        {label}
       </label>
       <input
         type="text"
-        name="name"
-        id="name"
+        name={name}
+        id={id}
         className="block w-full rounded-md border-0 py-1.5 px-2 text-dark shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-dark sm:text-sm sm:leading-6"
-        // placeholder={props.placeholder || "Jane Smith"}
+        value={value}
+        onChange={onChange}
       />
     </div>
   );
