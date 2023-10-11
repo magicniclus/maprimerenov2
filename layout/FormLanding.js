926eec40fa7113bdec1a5c657817b4727cc98073
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-10-11 22:11:31 +0200

    Update FormLanding.js

--- layout/FormLanding.js
+++ layout/FormLanding.js
@@ -5,11 +5,9 @@ const FormLanding = (props) => {
     <div className="flex flex-col items-center w-full">
       <h3 className="mt-8 mb-2 text-dark text-md font-bold ">{props.title}</h3>
       {props.description && (
-        <p className="text-center text-xs text-dark/50 mb-8">
-          {props.description}
-        </p>
+        <p className="text-center text-xs text-dark/50">{props.description}</p>
       )}
-      <div className="flex min-w-full justify-around flex-wrap">
+      <div className="flex min-w-full justify-around flex-wrap mt-5">
         {props.children}
       </div>
     </div>
