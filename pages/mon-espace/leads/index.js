2d5138897478d2024878bb89d59bbf712e2d3485
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-10-24 16:39:10 +0200

    Update index.js

--- pages/mon-espace/leads/index.js
+++ pages/mon-espace/leads/index.js
@@ -1,8 +1,37 @@
-import React from "react";
+import Table from "../../../components/partner/Table";
+import Pagination from "../../../components/partner/Pagination";
+import { useSelector, useDispatch } from "react-redux";
+import React, { use, useEffect, useState } from "react";
+import { watchContacts } from "../../../firebase/dataManager";
 import Pertner from "../../../layout/Pertner";
 
 const index = () => {
-  return <Pertner></Pertner>;
+  const getPagination = useSelector((state) => state.pagination);
+  const [contacts, setContacts] = useState([]);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const unsubscribe = watchContacts(getPagination, (newContacts) => {
+      console.log("New contacts received:", newContacts);
+      console.log("Setting contacts in redux:", contacts);
+      setContacts(newContacts);
+    });
+
+    // Lorsque le composant est démonté, arrêtez d'écouter les changements
+    return () => unsubscribe();
+  }, [getPagination]);
+
+  useEffect(() => {
+    dispatch({ type: "SET_CONTACTS", payload: contacts });
+  }, [contacts]);
+
+  return (
+    <Pertner>
+      <Table />
+      <Pagination />
+    </Pertner>
+  );
 };
 
 export default index;
