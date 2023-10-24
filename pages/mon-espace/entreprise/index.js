4bb451fa99ef45d1d973d734bd4b491e2fdc205d
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-10-24 19:12:50 +0200

    Update index.js

--- pages/mon-espace/entreprise/index.js
+++ pages/mon-espace/entreprise/index.js
@@ -1,8 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Pertner from "../../../layout/Pertner";
+import { watchCompanies } from "../../../firebase/partnerManager";
+import SectionWithAction from "../../../components/partner/SectionWithAction";
+import TableOfPartner from "../../../components/partner/TableOfPartner";
 
 const index = () => {
-  return <Pertner></Pertner>;
+  const [entreprises, setEntreprises] = useState([]);
+
+  useEffect(() => {
+    const unsubscribe = watchCompanies("33", (companies) => {
+      console.log("Companies in sector 33: ", companies);
+      setEntreprises(companies);
+    });
+    return () => unsubscribe();
+  }, []);
+
+  return (
+    <Pertner>
+      <SectionWithAction />
+      <TableOfPartner />
+    </Pertner>
+  );
 };
 
 export default index;
