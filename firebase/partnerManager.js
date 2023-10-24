0bda67e7242ecab9e1239ac3b64f1a01e25d119d
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-10-24 16:33:38 +0200

    Update partnerManager.js

--- firebase/partnerManager.js
+++ firebase/partnerManager.js
@@ -1,4 +1,8 @@
-import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+} from "firebase/auth";
 import app from "./firebase.config"; // Remplacez par le chemin correct vers votre fichier de configuration Firebase
 
 export const authenticateWithFirebase = (email, password) => {
@@ -12,5 +16,19 @@ export const authenticateWithFirebase = (email, password) => {
       .catch((error) => {
         reject(error);
       });
+  });
+};
+
+export const isUserAuthenticated = () => {
+  const auth = getAuth(app);
+
+  return new Promise((resolve, reject) => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        resolve(true); // L'utilisateur est connecté
+      } else {
+        resolve(false); // L'utilisateur n'est pas connecté
+      }
+    });
   });
 };
