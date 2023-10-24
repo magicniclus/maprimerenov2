b37694c59d6d8256941f8730e19cec50b81728d8
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-10-24 16:33:36 +0200

    Update dataManager.js

--- firebase/dataManager.js
+++ firebase/dataManager.js
@@ -1,6 +1,19 @@
 import { Database } from "./firebase.config.js";
-import { ref, push, set } from "firebase/database";
+import {
+  ref,
+  push,
+  set,
+  get,
+  onValue,
+  off,
+  getFirestore,
+  query,
+  collection,
+  orderBy,
+  onSnapshot,
+} from "firebase/database";
 import { database } from "./firebase.config";
+import { convertDateToISO } from "../utils/convertDateToISO.js";
 
 export const updateUserData = async (userData) => {
   try {
@@ -34,6 +47,70 @@ export const updateContactData = async (userData) => {
     console.log("Data sent successfully!");
   } catch (error) {
     console.error("Error sending data: ", error);
+    throw error;
+  }
+};
+
+export const getAllContacts = async () => {
+  try {
+    const dbRef = ref(database, "prospect");
+    const snapshot = await get(dbRef);
+
+    if (snapshot.exists()) {
+      return snapshot.val();
+    } else {
+      console.log("No contacts found");
+      return {};
+    }
+  } catch (error) {
+    console.error("Error fetching contacts: ", error);
+    throw error;
+  }
+};
+
+export const watchContacts = (pageNumber, callback) => {
+  const numberOfDocumentsPerPage = 10;
+  const startAtVal = (pageNumber - 1) * numberOfDocumentsPerPage;
+  const endAtVal = pageNumber * numberOfDocumentsPerPage - 1;
+
+  const contactsRef = ref(database, "prospect");
+
+  const listener = onValue(contactsRef, (snapshot) => {
+    let contactsArray = [];
+    const data = snapshot.val();
+
+    if (data) {
+      contactsArray = Object.values(data)
+        .sort((a, b) => {
+          const dateA = new Date(convertDateToISO(a.date));
+          const dateB = new Date(convertDateToISO(b.date));
+          return dateB - dateA; // Tri décroissant
+        })
+        .slice(startAtVal, endAtVal + 1);
+
+      console.log("Data from Firebase:", contactsArray);
+    }
+
+    callback(contactsArray);
+    console.log("Data from Firebase:", data);
+  });
+
+  return () => off(contactsRef, "value", listener);
+};
+
+export const getTotalContactsCount = async () => {
+  try {
+    const dbRef = ref(database, "prospect");
+    const snapshot = await get(dbRef);
+
+    if (snapshot.exists()) {
+      return Object.keys(snapshot.val()).length; // Renvoie la taille des contacts
+    } else {
+      console.log("No contacts found");
+      return 0;
+    }
+  } catch (error) {
+    console.error("Error fetching contacts count: ", error);
     throw error;
   }
 };
