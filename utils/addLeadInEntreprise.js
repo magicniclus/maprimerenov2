625a684f52f38125da6cac2e8f83e9af1448c244
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-10-25 17:53:09 +0200

    Create addLeadInEntreprise.js

---
+++ utils/addLeadInEntreprise.js
@@ -0,0 +1,42 @@
+import {
+  getAllPartnersBySector,
+  updateCurrentIndex,
+  getCurrentIndex,
+  addProspectToPartner,
+} from "../firebase/partnerManager";
+
+export const addLeadInEntreprise = async (userData) => {
+  return new Promise(async (resolve, reject) => {
+    const parteners = await getAllPartnersBySector();
+    const codeSector = userData.codePostal.substring(0, 2);
+
+    if (parteners.hasOwnProperty(codeSector)) {
+      let matchedPartners = Object.values(parteners[codeSector]);
+
+      for (let travail of userData.travaux) {
+        let partnersForCurrentJob = matchedPartners.filter((partner) =>
+          partner.prestation.includes(travail)
+        );
+
+        if (partnersForCurrentJob.length > 0) {
+          let currentIndex = await getCurrentIndex(codeSector);
+          const selectedPartner =
+            partnersForCurrentJob[currentIndex % partnersForCurrentJob.length];
+
+          // Ajoutez le prospect à la base de données du partenaire sélectionné
+          await addProspectToPartner(selectedPartner.id, userData);
+
+          await updateCurrentIndex(
+            codeSector,
+            (currentIndex + 1) % partnersForCurrentJob.length
+          );
+        }
+      }
+
+      resolve();
+    } else {
+      console.log("No matching partners found for this codePostal.");
+      resolve(null);
+    }
+  });
+};
