d51ee44c45a5dc0feb0262c97c9c0563b80a9ebb
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-10-25 10:23:23 +0200

    Update SectionWithAction.js

--- components/partner/SectionWithAction.js
+++ components/partner/SectionWithAction.js
@@ -1,4 +1,5 @@
 import React from "react";
+import ButtonGreen from "../button/ButtonGreen";
 
 const SectionWithAction = () => {
   const openModal = (e) => {
@@ -8,13 +9,13 @@ const SectionWithAction = () => {
   return (
     <div className="border-b border-gray-200 pb-5 sm:flex sm:items-center sm:justify-between">
       <h3 className="text-base font-semibold leading-6 text-gray-900">
-        Entreprises
+        Ajouter une entreprise
       </h3>
       <div className="mt-3 flex sm:ml-4 sm:mt-0">
         <a
           type="button"
           href="entreprise/create-partner"
-          className="ml-3 inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          className="ml-3 inline-flex items-center rounded-md bg-green px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-green/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green"
         >
           Ajouter
         </a>
