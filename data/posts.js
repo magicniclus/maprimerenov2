d00c12351d5c0fbfddf0cbc14077d610ae5d4694
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-10-27 18:49:52 +0200

    Create posts.js

---
+++ data/posts.js
@@ -0,0 +1,183 @@
+export const posts = [
+  {
+    id: 1,
+    slug: "chaudiere-bois",
+    title: "Les avantages de la chaudière à bois",
+    description:
+      "Découvrez pourquoi la chaudière à bois est devenue un choix populaire pour le chauffage domestique et ses avantages en matière d'efficacité et d'environnement.",
+    type: "conseils-travaux",
+    author: "Nicolas Castera",
+    readingTime: "4 min",
+    content: [
+      {
+        type: "title",
+        value: "Les avantages de la chaudière à bois",
+      },
+      {
+        type: "subtitle",
+        value: "Un choix éco-responsable",
+      },
+      {
+        type: "paragraph",
+        value:
+          "La chaudière à bois est non seulement un excellent moyen de réduire les coûts de chauffage, mais elle est également respectueuse de l'environnement. Contrairement aux combustibles fossiles, le bois est une ressource renouvelable.",
+      },
+      {
+        type: "paragraph",
+        value:
+          "De plus, les chaudières modernes ont amélioré leur efficacité, ce qui signifie moins de gaspillage et une combustion plus propre.",
+      },
+      {
+        type: "subtitle",
+        value: "Economie et efficacité",
+      },
+      {
+        type: "paragraph",
+        value:
+          "L'une des raisons pour lesquelles de nombreux propriétaires choisissent la chaudière à bois est l'économie potentielle sur les factures de chauffage. En utilisant du bois, souvent moins cher que les autres combustibles, les coûts peuvent être considérablement réduits.",
+      },
+      {
+        type: "list",
+        value: [
+          "Réduction des coûts de chauffage",
+          "Efficacité améliorée avec les modèles modernes",
+          "Combustion propre avec moins d'émissions",
+        ],
+      },
+      {
+        type: "subtitle",
+        value: "Conclusion",
+      },
+      {
+        type: "paragraph",
+        value:
+          "La chaudière à bois est donc une solution avantageuse à la fois pour l'environnement et pour le portefeuille. Si vous envisagez une nouvelle solution de chauffage, c'est certainement une option à considérer.",
+      },
+    ],
+    imageUrl: "/path/to/your/image/chaudiere-bois.jpg",
+  },
+  {
+    id: 2,
+    slug: "chauffage-gaz",
+    title: "Les avantages du chauffage au gaz pour votre maison",
+    description:
+      "Explorez les bénéfices majeurs du chauffage au gaz, une solution économique et efficace pour les foyers modernes. Découvrez comment il se compare aux autres méthodes de chauffage.",
+    type: "conseils-travaux",
+    author: "Nicolas Castera",
+    readingTime: "4 min",
+    content: [
+      {
+        type: "title",
+        value: "Les avantages du chauffage au gaz pour votre maison",
+      },
+      {
+        type: "subtitle",
+        value: "Performance et efficacité",
+      },
+      {
+        type: "paragraph",
+        value:
+          "Le chauffage au gaz est reconnu pour sa capacité à chauffer rapidement les espaces. Grâce à la rapidité de la combustion du gaz, votre maison peut atteindre la température désirée en un rien de temps.",
+      },
+      {
+        type: "paragraph",
+        value:
+          "Les appareils de chauffage au gaz d'aujourd'hui sont également conçus pour être hautement efficaces, garantissant que le moins de gaz possible est gaspillé lors de la combustion.",
+      },
+      {
+        type: "subtitle",
+        value: "Coût avantageux et écologie",
+      },
+      {
+        type: "paragraph",
+        value:
+          "Comparativement à l'électricité, le gaz naturel est souvent moins cher, ce qui permet de réaliser des économies substantielles sur les factures de chauffage. De plus, les appareils modernes peuvent avoir une combustion plus propre, réduisant ainsi les émissions nocives.",
+      },
+      {
+        type: "list",
+        value: [
+          "Rapidité de chauffe",
+          "Haute efficacité des appareils modernes",
+          "Economies sur les factures de chauffage",
+        ],
+      },
+      {
+        type: "subtitle",
+        value: "Conclusion",
+      },
+      {
+        type: "paragraph",
+        value:
+          "Opter pour le chauffage au gaz est une décision judicieuse pour ceux qui cherchent une méthode de chauffage fiable, économique et respectueuse de l'environnement. Pensez-y lors de la rénovation ou la construction de votre maison.",
+      },
+    ],
+    imageUrl: "/path/to/your/image/chauffage-gaz.jpg",
+  },
+  {
+    id: 3,
+    slug: "isolation-murs-interieur",
+    title: "L'importance de l'isolation des murs intérieurs pour votre habitat",
+    description:
+      "Découvrez comment l'isolation des murs intérieurs peut augmenter l'efficacité énergétique de votre maison tout en réduisant vos factures. Apprenez les avantages et les méthodes d'isolation.",
+    type: "conseils-travaux",
+    author: "Nicolas CASTERA",
+    readingTime: "4 min",
+    content: [
+      {
+        type: "title",
+        value:
+          "L'importance de l'isolation des murs intérieurs pour votre habitat",
+      },
+      {
+        type: "subtitle",
+        value: "Pourquoi isoler les murs intérieurs?",
+      },
+      {
+        type: "paragraph",
+        value:
+          "L'isolation des murs intérieurs joue un rôle crucial dans le maintien d'une température ambiante confortable chez vous. Elle aide à conserver la chaleur pendant l'hiver et à garder votre maison fraîche pendant l'été.",
+      },
+      {
+        type: "paragraph",
+        value:
+          "Une isolation adéquate des murs intérieurs peut également réduire les nuisances sonores provenant de l'extérieur, créant ainsi un environnement paisible à l'intérieur de votre domicile.",
+      },
+      {
+        type: "subtitle",
+        value: "Avantages économiques et écologiques",
+      },
+      {
+        type: "paragraph",
+        value:
+          "Isoler les murs intérieurs est un investissement rentable à long terme. En améliorant l'efficacité thermique de votre maison, vous pouvez réaliser des économies significatives sur vos factures d'énergie.",
+      },
+      {
+        type: "list",
+        value: [
+          "Réduction des coûts de chauffage et de climatisation",
+          "Amélioration de l'efficacité énergétique du domicile",
+          "Contribution à la protection de l'environnement en diminuant la consommation d'énergie",
+        ],
+      },
+      {
+        type: "subtitle",
+        value: "Choisir la bonne méthode d'isolation",
+      },
+      {
+        type: "paragraph",
+        value:
+          "Il existe différentes méthodes d'isolation des murs intérieurs, telles que l'isolation par laine minérale, par mousse expansive ou par panneaux isolants rigides. Il est essentiel de consulter un professionnel pour déterminer la meilleure solution adaptée à votre habitation.",
+      },
+      {
+        type: "subtitle",
+        value: "Conclusion",
+      },
+      {
+        type: "paragraph",
+        value:
+          "L'isolation des murs intérieurs est une étape essentielle pour améliorer le confort de votre maison tout en faisant des économies d'énergie. C'est également une démarche éco-responsable favorisant la réduction de l'empreinte carbone. N'attendez plus pour renforcer l'isolation de votre domicile!",
+      },
+    ],
+    imageUrl: "/path/to/your/image/isolation-murs-interieur.jpg",
+  },
+];
