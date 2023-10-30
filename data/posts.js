0206eb76dc6e914abfe7d9847feb6fdcd5192adf
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-10-30 13:24:50 +0100

    Update posts.js

--- data/posts.js
+++ data/posts.js
@@ -113,7 +113,7 @@ export const posts = [
       },
     ],
     imageUrl:
-      "https://plus.unsplash.com/premium_photo-1661301068444-8ac48208d017?auto=format&fit=crop&q=80&w=1587&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      "https://images.unsplash.com/photo-1614792403436-ba5b3e747604?auto=format&fit=crop&q=60&w=500&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8Q0hBVURJRVJFfGVufDB8fDB8fHww",
   },
   {
     id: 3,
