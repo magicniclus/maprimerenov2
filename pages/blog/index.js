9b5720a841875117870421d0aac4bc9c3a20c0bc
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-10-30 10:13:35 +0100

    Update index.js

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,113 +1,14 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Basic from "../../layout/Basic";
 import CTA from "../../components/CTA/CTA";
-
-const posts = [
-  {
-    id: 1,
-    title: "Boost your conversion rate",
-    href: "#",
-    description:
-      "Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.",
-    imageUrl: "/images/imgs/istock.png",
-    date: "Mar 16, 2020",
-    datetime: "2020-03-16",
-    category: { title: "Marketing", href: "#" },
-    author: {
-      name: "Michael Foster",
-      role: "Co-Founder / CTO",
-      href: "#",
-      imageUrl: "/images/imgs/istock.png",
-    },
-  },
-  {
-    id: 2,
-    title: "Boost your conversion rate",
-    href: "#",
-    description:
-      "Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.",
-    imageUrl: "/images/imgs/istock.png",
-    date: "Mar 16, 2020",
-    datetime: "2020-03-16",
-    category: { title: "Marketing", href: "#" },
-    author: {
-      name: "Michael Foster",
-      role: "Co-Founder / CTO",
-      href: "#",
-      imageUrl: "/images/imgs/istock.png",
-    },
-  },
-  {
-    id: 3,
-    title: "Boost your conversion rate",
-    href: "#",
-    description:
-      "Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.",
-    imageUrl: "/images/imgs/istock.png",
-    date: "Mar 16, 2020",
-    datetime: "2020-03-16",
-    category: { title: "Marketing", href: "#" },
-    author: {
-      name: "Michael Foster",
-      role: "Co-Founder / CTO",
-      href: "#",
-      imageUrl: "/images/imgs/istock.png",
-    },
-  },
-  {
-    id: 4,
-    title: "Boost your conversion rate",
-    href: "#",
-    description:
-      "Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.",
-    imageUrl: "/images/imgs/istock.png",
-    date: "Mar 16, 2020",
-    datetime: "2020-03-16",
-    category: { title: "Marketing", href: "#" },
-    author: {
-      name: "Michael Foster",
-      role: "Co-Founder / CTO",
-      href: "#",
-      imageUrl: "/images/imgs/istock.png",
-    },
-  },
-  {
-    id: 5,
-    title: "Boost your conversion rate",
-    href: "#",
-    description:
-      "Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.",
-    imageUrl: "/images/imgs/istock.png",
-    date: "Mar 16, 2020",
-    datetime: "2020-03-16",
-    category: { title: "Marketing", href: "#" },
-    author: {
-      name: "Michael Foster",
-      role: "Co-Founder / CTO",
-      href: "#",
-      imageUrl: "/images/imgs/istock.png",
-    },
-  },
-  {
-    id: 6,
-    title: "Boost your conversion rate",
-    href: "#",
-    description:
-      "Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.",
-    imageUrl: "/images/imgs/istock.png",
-    date: "Mar 16, 2020",
-    datetime: "2020-03-16",
-    category: { title: "Marketing", href: "#" },
-    author: {
-      name: "Michael Foster",
-      role: "Co-Founder / CTO",
-      href: "#",
-      imageUrl: "/images/imgs/istock.png",
-    },
-  },
-];
+import { posts } from "../../data/posts";
 
 const index = () => {
+  const [articles, setArticles] = useState("");
+  useEffect(() => {
+    setArticles(posts);
+  }, [posts]);
+
   return (
     <Basic>
       <section className="min-h-[calc(100vh-104px)] max-w-[1250px] mx-auto px-5 lg:pb-10 lg:px-20 flex flex-col">
@@ -122,61 +23,55 @@ const index = () => {
           </h2>
         </div>
         <div className="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-3">
-          {posts.map((post) => (
-            <article
-              key={post.id}
-              className="flex flex-col items-start justify-between"
-            >
-              <div className="relative w-full">
-                <img
-                  src={post.imageUrl}
-                  alt=""
-                  className="aspect-[16/9] w-full rounded-2xl bg-gray-100 object-cover sm:aspect-[2/1] lg:aspect-[3/2]"
-                />
-                <div className="absolute inset-0 rounded-2xl ring-1 ring-inset ring-gray-900/10" />
-              </div>
-              <div className="max-w-xl">
-                <div className="mt-8 flex items-center gap-x-4 text-xs">
-                  <time dateTime={post.datetime} className="text-dark/75">
-                    {post.date}
-                  </time>
-                  <a
-                    href={post.category.href}
-                    className="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-dark hover:bg-gray-100"
-                  >
-                    {post.category.title}
-                  </a>
+          {articles &&
+            articles.map((post) => (
+              <article
+                key={post.id}
+                className="flex flex-col items-start justify-between"
+              >
+                <div className="relative w-full">
+                  <img
+                    src={post.imageUrl}
+                    alt=""
+                    className="aspect-[16/9] w-full rounded-2xl bg-gray-100 object-cover sm:aspect-[2/1] lg:aspect-[3/2]"
+                  />
+                  <div className="absolute inset-0 rounded-2xl ring-1 ring-inset ring-gray-900/10" />
                 </div>
-                <div className="group relative">
-                  <h3 className="mt-3 text-lg font-semibold leading-6 text-dark group-hover:text-gray-600">
-                    <a href={post.href}>
-                      <span className="absolute inset-0" />
+                <div className="max-w-xl">
+                  <div className="mt-8 flex items-center gap-x-4 text-xs">
+                    <time dateTime={post.datetime} className="text-dark/75">
+                      {post.date}
+                    </time>
+                    <a
+                      href={`blog/${post.type}/${post.slug}`}
+                      className="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-dark hover:bg-gray-100"
+                    >
                       {post.title}
                     </a>
-                  </h3>
-                  <p className="mt-5 line-clamp-3 text-sm leading-6 text-dark">
-                    {post.description}
-                  </p>
-                </div>
-                <div className="relative mt-8 flex items-center gap-x-4">
-                  <img
-                    src={post.author.imageUrl}
-                    alt=""
-                    className="h-10 w-10 rounded-full bg-gray-100"
-                  />
-                  <div className="text-sm leading-6">
-                    <p className="font-semibold text-dark">
-                      <a href={post.author.href}>
+                  </div>
+                  <div className="group relative">
+                    <h3 className="mt-3 text-lg font-semibold leading-6 text-dark group-hover:text-gray-600">
+                      <a href={post.href}>
                         <span className="absolute inset-0" />
-                        {post.author.name}
+                        {post.title}
                       </a>
+                    </h3>
+                    <p className="mt-5 line-clamp-3 text-sm leading-6 text-dark">
+                      {post.description}
                     </p>
-                    <p className="text-gray-600">{post.author.role}</p>
+                    <div className="text-sm leading-6">
+                      <p className="font-semibold text-dark">
+                        <p>
+                          <span className="absolute inset-0" />
+                          {post.author}
+                        </p>
+                      </p>
+                      <p className="text-gray-600">{post.author.role}</p>
+                    </div>
                   </div>
                 </div>
-              </div>
-            </article>
-          ))}
+              </article>
+            ))}
         </div>
       </section>
       <CTA />
