86fbaf6fb20b3ab7bebfdc6c42e9f6720e619b8e
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-10-30 10:45:06 +0100

    Update BlogContainer.js

--- components/blogs/BlogContainer.js
+++ components/blogs/BlogContainer.js
@@ -1,58 +1,12 @@
 import React from "react";
-const posts = [
-  {
-    id: 1,
-    title: "Boost your conversion rate",
-    href: "#",
-    description:
-      "Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.",
-    imageUrl: "/images/imgs/istock.png",
-    date: "Mar 16, 2020",
-    datetime: "2020-03-16",
-    category: { title: "Marketing", href: "#" },
-    author: {
-      name: "Michael Foster",
-      role: "Co-Founder / CTO",
-      href: "#",
-      imageUrl: "/images/imgs/istock.png",
-    },
-  },
-  {
-    id: 2,
-    title: "Boost your conversion rate",
-    href: "#",
-    description:
-      "Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.",
-    imageUrl: "/images/imgs/istock.png",
-    date: "Mar 16, 2020",
-    datetime: "2020-03-16",
-    category: { title: "Marketing", href: "#" },
-    author: {
-      name: "Michael Foster",
-      role: "Co-Founder / CTO",
-      href: "#",
-      imageUrl: "/images/imgs/istock.png",
-    },
-  },
-  {
-    id: 3,
-    title: "Boost your conversion rate",
-    href: "#",
-    description:
-      "Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.",
-    imageUrl: "/images/imgs/istock.png",
-    date: "Mar 16, 2020",
-    datetime: "2020-03-16",
-    category: { title: "Marketing", href: "#" },
-    author: {
-      name: "Michael Foster",
-      role: "Co-Founder / CTO",
-      href: "#",
-      imageUrl: "/images/imgs/istock.png",
-    },
-  },
-];
+import { posts } from "../../data/posts";
+import { useState, useEffect } from "react";
+
 const BlogContainer = () => {
+  const [articles, setArticles] = useState("");
+  useEffect(() => {
+    setArticles(posts);
+  }, [posts]);
   return (
     <section className="bg-white py-24 sm:py-32">
       <div className="mx-auto max-w-7xl px-6 lg:px-20">
@@ -65,7 +19,7 @@ const BlogContainer = () => {
           </p>
         </div>
         <div className="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-3">
-          {posts.map((post) => (
+          {posts.slice(0, 3).map((post) => (
             <article
               key={post.id}
               className="flex flex-col items-start justify-between"
@@ -80,14 +34,11 @@ const BlogContainer = () => {
               </div>
               <div className="max-w-xl">
                 <div className="mt-8 flex items-center gap-x-4 text-xs">
-                  <time dateTime={post.datetime} className="text-dark/75">
-                    {post.date}
-                  </time>
                   <a
-                    href={post.category.href}
+                    href={`blog/${post.type}/${post.slug}`}
                     className="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-dark hover:bg-gray-100"
                   >
-                    {post.category.title}
+                    {post.title}
                   </a>
                 </div>
                 <div className="group relative">
@@ -102,19 +53,13 @@ const BlogContainer = () => {
                   </p>
                 </div>
                 <div className="relative mt-8 flex items-center gap-x-4">
-                  <img
-                    src={post.author.imageUrl}
-                    alt=""
-                    className="h-10 w-10 rounded-full bg-gray-100"
-                  />
                   <div className="text-sm leading-6">
                     <p className="font-semibold text-dark">
-                      <a href={post.author.href}>
+                      <a href={`blog/${post.type}/${post.slug}`}>
                         <span className="absolute inset-0" />
-                        {post.author.name}
+                        {post.author}
                       </a>
                     </p>
-                    <p className="text-gray-600">{post.author.role}</p>
                   </div>
                 </div>
               </div>
