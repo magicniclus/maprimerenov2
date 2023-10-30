ab6f8e7dd69af3054d654c9ba2e77e4b49a06da0
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-10-30 10:13:29 +0100

    Update Basic.js

--- layout/Basic.js
+++ layout/Basic.js
@@ -3,40 +3,24 @@ import Head from "next/head";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 
-const Basic = (props) => {
+const Basic = ({ title, description, keywords, ogImage, ogUrl, children }) => {
   return (
     <>
       <Head>
-        <title>{props.title}</title>
-        <meta name="description" content={props.description} />
+        <title>{title}</title>
+        <meta name="description" content={description} />
         <link rel="icon" href="/favicon.ico" />
-        <meta
-          name="description"
-          content="Utilisez notre estimateur avancé pour connaître la valeur exacte de votre bien immobilier. Obtenez une estimation rapide, fiable et basée sur les données actuelles du marché avec Avenue-Immo."
-        />
-        <meta
-          name="keywords"
-          content="outil d'estimation immobilière, estimation de maison en ligne, estimation d'appartement, valeur de propriété, Avenue-Immo estimateur"
-        />
-        <meta
-          property="og:title"
-          content="Estimateur Avenue-Immo : Obtenez une Estimation Précise de Votre Bien"
-        />
-        <meta
-          property="og:description"
-          content="Utilisez notre estimateur avancé pour connaître la valeur exacte de votre bien immobilier. Obtenez une estimation rapide, fiable et basée sur les données actuelles du marché avec Avenue-Immo."
-        />
-        <meta
-          property="og:image"
-          content="URL_DE_VOTRE_IMAGE_REPRESENTANT_L_ESTIMATEUR"
-        />
-        <meta
-          property="og:url"
-          content="https://www.avenue-immo.com/estimation"
-        />
+        <meta name="keywords" content={keywords} />
+        <link rel="canonical" href={ogUrl} />
+
+        {/* Open Graph */}
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+        <meta property="og:image" content={ogImage} />
+        <meta property="og:url" content={ogUrl} />
       </Head>
       <Header />
-      <main className="">{props.children}</main>
+      <main className="">{children}</main>
       <Footer />
     </>
   );
