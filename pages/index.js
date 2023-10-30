b5f1d1fe58a51a96312347e7f92b9b9de2691a86
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-10-30 14:04:23 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,20 @@ import Basic from "../layout/Basic";
 
 export default function Home() {
   return (
-    <Basic>
+    <Basic
+      title="Accompagnement pour l'aide MaPrimeRenov | MaPrimeRenov-info" // 72 caractères
+      ogUrl="https://maprimerenov-info.org/" // URL de la page d'accueil
+      description="MaPrimeRenov-info vous guide pas à pas dans votre démarche pour obtenir l'aide MaPrimeRenov. Bénéficiez d'un accompagnement personnalisé pour vos projets de rénovation." // 159 caractères
+      keywords={[
+        "MaPrimeRenov",
+        "accompagnement",
+        "obtenir aide",
+        "guide MaPrimeRenov",
+        "soutien projet rénovation",
+        "aide rénovation énergétique",
+      ]}
+      ogImage="/images/logos/favicon.png"
+    >
       <Hero />
       <Feature />
       <CTA />
