43c9656f62c5d217c6a67a6056c9d282945d0682
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-11-02 15:46:07 +0100

    Update HeaderLanding.js

--- components/headers/HeaderLanding.js
+++ components/headers/HeaderLanding.js
@@ -4,7 +4,7 @@ const HeaderLanding = () => {
   return (
     <section className="bg-primary lg:bg-bg-landing bg-no-repeat bg-cover bg-center h-auto w-full min-h-[400px] bg-opacity-100 lg:bg-opacity-0  max-w-[1250px] mx-auto px-5 lg:pb-10 lg:px-20 flex justify-center flex-col">
       <h1 className="text-white mt-10 text-4xl tracking-tight sm:text-6xl font-light">
-        Testez votre éligibilité aux aides 2023 encadré par l’état:
+        Testez votre éligibilité aux aides 2023 encadré par l’état :
         MaPrimeRenov’!
       </h1>
       <h2 className="text-white my-10 text-2xl">
