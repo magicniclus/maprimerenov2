19f41b507b6062416fd37f732f4f9b453b31cf6a
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-11-03 10:15:58 +0100

    Update Pertner.js

--- layout/Pertner.js
+++ layout/Pertner.js
@@ -29,6 +29,7 @@ function classNames(...classes) {
 const Pertner = ({ children }) => {
   const router = useRouter();
   const [navItems, setNavItems] = useState(navigation);
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   useEffect(() => {
     const path = router.pathname;
@@ -161,7 +162,7 @@ const Pertner = ({ children }) => {
                 </div>
               </div>
 
-              {/* <Disclosure.Panel className="md:hidden">
+              <Disclosure.Panel className="md:hidden">
                 <div className="space-y-1 px-2 pb-3 pt-2 sm:px-3">
                   {navItems.map((item) => (
                     <Disclosure.Button
@@ -179,47 +180,49 @@ const Pertner = ({ children }) => {
                       {item.name}
                     </Disclosure.Button>
                   ))}
-                </div> */}
-              {/* <div className="border-t border-gray-700 pb-3 pt-4">
-                  <div className="flex items-center px-5">
-                    <div className="flex-shrink-0">
-                      <img
-                        className="h-10 w-10 rounded-full"
-                        src={user.imageUrl}
-                        alt=""
-                      />
-                    </div>
-                    <div className="ml-3">
-                      <div className="text-base font-medium text-white">
-                        {user.name}
+                </div>
+                {/* {
+                  <div className="border-t border-gray-700 pb-3 pt-4">
+                    <div className="flex items-center px-5">
+                      <div className="flex-shrink-0">
+                        <img
+                          className="h-10 w-10 rounded-full"
+                          src={user.imageUrl}
+                          alt=""
+                        />
                       </div>
-                      <div className="text-sm font-medium text-gray-400">
-                        {user.email}
+                      <div className="ml-3">
+                        <div className="text-base font-medium text-white">
+                          {user.name}
+                        </div>
+                        <div className="text-sm font-medium text-gray-400">
+                          {user.email}
+                        </div>
                       </div>
-                    </div>
-                    <button
-                      type="button"
-                      className="relative ml-auto flex-shrink-0 rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-                    >
-                      <span className="absolute -inset-1.5" />
-                      <span className="sr-only">View notifications</span>
-                      <BellIcon className="h-6 w-6" aria-hidden="true" />
-                    </button>
-                  </div>
-                  <div className="mt-3 space-y-1 px-2">
-                    {userNavigation.map((item) => (
-                      <Disclosure.Button
-                        key={item.name}
-                        as="a"
-                        href={item.href}
-                        className="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white"
+                      <button
+                        type="button"
+                        className="relative ml-auto flex-shrink-0 rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
                       >
-                        {item.name}
-                      </Disclosure.Button>
-                    ))}
+                        <span className="absolute -inset-1.5" />
+                        <span className="sr-only">View notifications</span>
+                        <BellIcon className="h-6 w-6" aria-hidden="true" />
+                      </button>
+                    </div>
+                    <div className="mt-3 space-y-1 px-2">
+                      {userNavigation.map((item) => (
+                        <Disclosure.Button
+                          key={item.name}
+                          as="a"
+                          href={item.href}
+                          className="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white"
+                        >
+                          {item.name}
+                        </Disclosure.Button>
+                      ))}
+                    </div>
                   </div>
-                </div> */}
-              {/* </Disclosure.Panel> */}
+                } */}
+              </Disclosure.Panel>
             </>
           )}
         </Disclosure>
